9498b9b5e47dbc2eece51618b3741644170fd28a
JianboYang <jyan0173@student.monash.edu>
2022-09-01 16:36:22 +0800

    Add project files.

---
+++ FIT5032_WEEK5_MyIdentity/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FIT5032_WEEK5_MyIdentity.MvcApplication" Language="C#" %>
---
+++ FIT5032_WEEK5_MyIdentity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIT5032_WEEK5_MyIdentity.Startup))]
+namespace FIT5032_WEEK5_MyIdentity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
